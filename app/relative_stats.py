2b2fe67c9a539067b11ed4e7567e175a45a83916
rfarrelly <rfarrelly03@qub.ac.uk>
2025-12-08 11:23:19 +0000

    Cleaner way to generate PPG

--- app/relative_stats.py
+++ app/relative_stats.py
@@ -15,66 +15,72 @@ def compute_points(df: DF) -> DF:
     return df
 
 
-def compute_ppg(df: DF) -> DF:
-    teams = set(df["Home"]).union(df["Away"])
+def compute_ppg(df: DF, shift: bool = 0) -> DF:
+    df = df.sort_values(["Home", "Date"])
 
-    home_points = df.pivot(index=["Home"], columns="Date", values="HP")
-    away_points = df.pivot(index=["Away"], columns="Date", values="AP")
-
-    ppg_list: List[DF] = []
-
-    for team in teams:
-        h_ppg = home_points.loc[team].sort_index().dropna().reset_index()
-        h_ppg["Home"] = team
-        h_ppg["HomePPG"] = h_ppg[team].expanding().mean().round(2)
-        h_ppg = h_ppg.rename(columns={team: "HP"})
-        h_ppg = h_ppg[["Date", "Home", "HP", "HomePPG"]]
-
-        a_ppg = away_points.loc[team].sort_index().dropna().reset_index()
-        a_ppg["Away"] = team
-        a_ppg["AwayPPG"] = a_ppg[team].expanding().mean().round(2)
-        a_ppg = a_ppg.rename(columns={team: "AP"})
-        a_ppg = a_ppg[["Date", "Away", "AP", "AwayPPG"]]
-
-        ppg = pd.concat([h_ppg, a_ppg], axis=0).sort_values("Date")
-        ppg["HA_PPG"] = ppg["HomePPG"].fillna(ppg["AwayPPG"])
-        ppg["TotalPPG"] = ppg["HP"].fillna(ppg["AP"]).expanding().mean().round(2)
-        ppg["Team"] = team
-        ppg_list.append(ppg)
-
-    return (
-        pd.concat(ppg_list)[["Date", "Team", "HA_PPG", "TotalPPG"]]
-        .pivot(index="Team", columns="Date", values="HA_PPG")
-        .sort_index()
-        .reset_index()
+    df["HPPG"] = (
+        df.groupby("Home")["HP"]
+        .expanding()
+        .mean()
+        .shift(shift)
+        .round(2)
+        .reset_index(level=0, drop=True)
     )
 
+    df = df.sort_values(["Away", "Date"])
 
-def opponents_ppg(team: str, df: DF, ppg_df: DF) -> List[str]:
-    team_df = df[(df["Home"] == team) | (df["Away"] == team)].copy()
-
-    team_df["Opp"] = np.where(
-        team_df["Home"] != team,
-        team_df["Home"],
-        np.where(team_df["Away"] != team, team_df["Away"], np.nan),
+    df["APPG"] = (
+        df.groupby("Away")["AP"]
+        .expanding()
+        .mean()
+        .shift(shift)
+        .round(2)
+        .reset_index(level=0, drop=True)
     )
 
-    ppg_df = ppg_df.merge(team_df, left_on="Team", right_on="Opp")[
-        ["Date"] + ["Team"] + team_df["Date"].to_list()
-    ]
+    home_df = df[["Date", "Home", "HP"]].rename(
+        columns={"Home": "Team", "HP": "Points"}
+    )
 
-    ppg_df.iloc[:, 2:] = ppg_df.iloc[:, 2:].expanding().mean()
+    away_df = df[["Date", "Away", "AP"]].rename(
+        columns={"Away": "Team", "AP": "Points"}
+    )
 
-    diags = np.diag(ppg_df.iloc[:, 2:])
+    long_df = pd.concat([home_df, away_df], ignore_index=True)
+
+    long_df = long_df.sort_values(["Team", "Date"])
+
+    long_df["TPPG"] = (
+        long_df.groupby("Team")["Points"]
+        .expanding()
+        .mean()
+        .round(2)
+        .reset_index(level=0, drop=True)
+    )
+
+    df = (
+        df.merge(
+            long_df, left_on=["Date", "Home", "HP"], right_on=["Date", "Team", "Points"]
+        )
+        .rename(columns={"TPPG": "HomeTotalPPG"})
+        .drop(["Team", "Points"], axis=1)
+    )
+
+    df = (
+        df.merge(
+            long_df, left_on=["Date", "Away", "AP"], right_on=["Date", "Team", "Points"]
+        )
+        .rename(columns={"TPPG": "AwayTotalPPG"})
+        .drop(["Team", "Points"], axis=1)
+    ).sort_values("Date")
+
     breakpoint()
 
 
 def main():
     points_df = compute_points(df)
-    ppg_df = compute_ppg(points_df)
-    opps = opponents_ppg("Aldershot Town", df, ppg_df)
 
-    # compute_ppi("Aldershot Town", ppg_df, opps)
+    ppg_df = compute_ppg(points_df, shift=0)
 
 
 if __name__ == "__main__":
